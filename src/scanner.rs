b2f2d014f709944c71385e4e9cade2604f97155b
sebastian <msagnik62@gmail.com>
2026-01-19 14:11:32 +0530

    added scanner.rs

---
+++ src/scanner.rs
@@ -0,0 +1,50 @@
+use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
+use std::ops::RangeInclusive;
+use std::thread::spawn;
+use std::time::Duration;
+
+#[allow(unused)]
+pub fn scan_ports(target: Ipv4Addr, ports: RangeInclusive<u16>, thread_count: usize) -> Vec<u16> {
+    let total_ports: u16 = ports.end() - ports.start() + 1;
+
+    // ceil division
+    let ports_per_thread = (total_ports as usize + thread_count - 1) / thread_count;
+
+    // The .start() and .end() returns &Idx, in this case &u16, so deferencing it is necessary
+    let mut handles: Vec<std::thread::JoinHandle<Vec<u16>>> = vec![];
+    let start_port = *ports.start();
+    let end_port = *ports.end();
+
+    // create the spliting of ports for threading
+    for i in 0..thread_count {
+        let chunk_start: u16 = start_port + (i * ports_per_thread) as u16;
+        let chunk_end: u16 = std::cmp::min(chunk_start + ports_per_thread as u16 - 1, end_port);
+
+        let handler: std::thread::JoinHandle<Vec<u16>> = spawn(move || {
+            let mut open_ports: Vec<u16> = vec![];
+
+            for port in chunk_start..=chunk_end {
+                let sock_addr = SocketAddrV4::new(target, port);
+
+                if TcpStream::connect_timeout(&sock_addr.into(), Duration::from_millis(500)).is_ok()
+                {
+                    open_ports.push(port);
+                }
+            }
+            open_ports
+        });
+        handles.push(handler);
+    }
+
+    // collect results from thread
+    let mut result = vec![];
+
+    for handle in handles {
+        let open_ports = handle.join().unwrap();
+        result.extend(open_ports);
+    }
+
+    result.sort_unstable(); // sort in ascending order - slightly faster than sort()
+
+    result // All open ports
+}
